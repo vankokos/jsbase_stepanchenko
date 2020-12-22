cb649b56c9d31da6a9c61f3728b73c909603a09b
vankokos <vano9995@gmail.com>
2020-12-22 17:26:21 +0200

    hotfix

--- hw/hw#1/js/script4.js
+++ hw/hw#1/js/script4.js
@@ -1,12 +1,17 @@
 var userName = prompt ("Как вас зовут?");
+console.log ("userName")
 var userSurname = prompt ("Какая ваша фамилия?");
+console.log ("userSurname")
 var userAge = prompt ("Какой ваш возраст?");
+console.log ("userAge")
 var userAgeVar;
+console.log ("userAgeVar")
 
 function userAgeVar(number, titles) {
     cases = [2, 0, 1, 1, 1, 2];  
     return number+" "+titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
 }
+console.log ("userAgeVarDeclination")
 
 var userInfo = alert( "Ваше имя - " + userName + ", Ваша фамилия - " + userSurname + ", Ваш возраст - " + userAgeVar(userAge, ['год', 'года', 'лет']) );
-
+console.log ("userInfo")
--- hw/hw#1/js/script5.js
+++ hw/hw#1/js/script5.js
@@ -1,9 +1,11 @@
 
 var age = prompt ("Введите ваш возраст");
+console.log ("age")
 
 if (confirm("Ваш возраст " + age + "?")){
      alert("true");
 } else {
     alert("false");
 }
+console.log ("ageConfirm")
 
